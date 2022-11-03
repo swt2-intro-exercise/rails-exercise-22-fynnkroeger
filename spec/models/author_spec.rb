7084fce261ddc1a3412d79127fe3a607ac4ae39b
Fynn <fynn_kroeger@web.de>
2022-11-03 15:59:38 +0100

    Completed Author for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,8 @@
 describe "Author object", type: :model do
     it "should have a working constructor" do
-        author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://en.wikipedia.org/wiki/Alan_Turing")
         expect(author.first_name).to eq("Alan")
+        expect(author.homepage).to eq("https://en.wikipedia.org/wiki/Alan_Turing")
+        expect(author.name).to eq("Alan Turing")
     end
 end
