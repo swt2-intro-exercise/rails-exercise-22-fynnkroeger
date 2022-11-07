dac5d718f2f6d34c8c11d1647e1bfd51238830ef
Fynn <fynn_kroeger@web.de>
2022-11-07 12:22:28 +0100

    Complete #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,6 +5,11 @@ class AuthorsController < ApplicationController
     def create
         @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
         @author.save
-        redirect_to root_path, notice: 'Success!'
+        # redirect_to root_path, notice: 'Success!'
+        redirect_to @author
+    end
+
+    def show
+        @author = Author.find(params[:id])
     end
 end
