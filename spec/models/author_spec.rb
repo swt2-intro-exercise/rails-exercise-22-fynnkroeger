69b22cdbf728a793003899f45de653df9f6d3086
Fynn <fynn_kroeger@web.de>
2022-11-11 08:52:42 +0100

    Failing test for #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,10 @@ describe "Author object", type: :model do
         expect(author.homepage).to eq("https://en.wikipedia.org/wiki/Alan_Turing")
         expect(author.name).to eq("Alan Turing")
     end
+    it "should not validate without last name" do
+        author = Author.new(first_name: "anna", last_name: nil, homepage: "example.com")
+        expect(author).to_not be_valid
+        author = Author.new(first_name: "john", last_name: "", homepage: "example.com")
+        expect(author).to_not be_valid
+    end
 end
