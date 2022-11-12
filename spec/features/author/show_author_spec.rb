11f52e729280a2402fba6d9f98d0d5c8e63c24d7
Fynn <fynn_kroeger@web.de>
2022-11-12 12:12:54 +0100

    Failing test for #10

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -9,4 +9,12 @@ describe "Author page", type: :feature do
         expect(page).to have_text("Alan Turing")
         expect(page).to have_text("http://wikipedia.de/Alan_Turing")
     end
+    it "should contian the linkt to delete the author" do
+        @alan = FactoryBot.create :author
+        visit author_path(@alan)
+        expect(page).to have_link("Delete")
+        old_count = Author.count
+        click_link("Delete")
+        expect(Author.count).to eq(old_count-1)
+    end
 end
