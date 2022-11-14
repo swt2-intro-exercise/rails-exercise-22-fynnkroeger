7b8fa193cc1893d8beb183a3a3a12daf86f09f2c
Fynn <fynn_kroeger@web.de>
2022-11-14 13:30:21 +0100

    Failing test for #14

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -7,4 +7,10 @@ describe "Paper index page", type: :feature do
         visit "/papers"
         expect(page).to have_link href: edit_paper_path(@paper)
     end
+
+    it "should have a link to delete paper" do
+        @paper = FactoryBot.create :paper
+        visit papers_path
+        expect(page).to have_link "Delete", href: paper_path(@paper)
+    end
 end
