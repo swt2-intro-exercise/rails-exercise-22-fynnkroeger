8cdd76ed9db346929caf23c14e283b251ce5c7e0
Fynn <fynn_kroeger@web.de>
2022-11-14 14:26:24 +0100

    Failing test for #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -4,5 +4,7 @@ describe "Paper object", type: :model do
         expect(paper).to_not be_valid
         paper2 = Paper.new(venue: 'hello', year: 1738)
         expect(paper2).to_not be_valid
+        paper3 = FactoryBot.create :paper
+        expect(paper3.authors.length).to eq(0)
     end
 end
